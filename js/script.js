2a8a6a36853f5477f7517ee9c909b0a53502516c
Abdullah Javed <>
2021-04-19 11:34:15 +0000

    ho aggiunto la lettura del file csv

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-import Chart from '/chart.js'
+
 
 const csvFileInput = document.querySelector("#csvFileInput");
 
@@ -30,22 +30,5 @@ csvFileInput.addEventListener("change", (e) => {
   });
 });
 
-const ctx = document.getElementById('my-chart').getContext('2d');
-const chart = new Chart(ctx, {
-  // The type of chart we want to create
-  type: 'bar',
 
-  // The data for our dataset
-  data: {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-    datasets: [{
-      label: 'My First dataset',
-      backgroundColor: 'rgb(255, 99, 132)',
-      borderColor: 'rgb(255, 99, 132)',
-      data: [0, 10, 5, 2, 20, 30, 45]
-    }]
-  },
 
-  // Configuration options go here
-  options: {}
-  });
