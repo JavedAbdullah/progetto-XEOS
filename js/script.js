0aa78ff0287ee9843c8ad9e01cde4bf3757937a4
Abdullah Javed <>
2021-04-22 14:22:57 +0000

    ho modificato i grafici, usando un'altra libreria per gestire 13k di dati

--- js/script.js
+++ js/script.js
@@ -18,32 +18,33 @@ csvFileInput.addEventListener("change", (e) => {
     // console.log( array_of_element.length)
     // console.log(array_of_title.length)
     //come contare gli elemnti nel array Javascript
-    let count = 0;  
-    let assex = [];
-    let assey = [];
-    let lableName = "xxy";
-    for(let i=1;i<array_of_title.length;i++){
-      for(let j=0;j<10;j++){
-          assex[j] = results.data[j][i];
 
-         let  milliseconds = results.data[j][0] * 1000 //1575909015000
+    let valore = [];
+
+    for(let i=1;i<array_of_title.length;i++){
+      for(let j=0;j<array_of_element.length;j++){
+          valore[j] = results.data[j][i];
+
+        //  let  milliseconds = results.data[j][0] * 1000 //1575909015000
          
 
-          let dateObject = new Date(milliseconds)
-          let humanDateFormat = dateObject.toLocaleString()
+        //   let dateObject = new Date(milliseconds)
+        //   let humanDateFormat = dateObject.toLocaleString()
           
-          assey[j] = humanDateFormat.slice(11);
+         // assey[j] = humanDateFormat.slice(11);
           
-          // console.log(assey)
+
 
       }
-     // console.log(array_of_title[i])
-     // console.log(assex.slice(1))
-      console.log(assey.slice(1))
-
-      count = 0;
+         let result =valore.map(i=>Number(i));
+    console.log(array_of_title[3]);
+     disegnaGrafico(result,array_of_title[i])
+      
     }
-   charit(assex.slice(1),assey.slice(1),array_of_title[3])
+    //console.log(assex)
+ 
+
+
 
     }
   });
@@ -53,34 +54,37 @@ csvFileInput.addEventListener("change", (e) => {
 
 
 
-// let x = [1,2,3,45,555]
-// let y = [23,43,1,32,43]
 
-function charit(x,y,labelName){
-let ctx = document.getElementById('chart').getContext('2d');
-let ctxFinal = ctx.fillRect(233, 227, 253,0);
-let myChart = new Chart( ctx, {
-    type: 'line',
-    data: {
-        labels: y,
-        datasets: [{
-            label: labelName,
-            data: x,
-            backgroundColor: 'rgba(255, 99, 132, 0.2)',
-            borderColor: 'rgba(255, 99, 132, 1)',
 
-            borderWidth: 2
+function disegnaGrafico(valoreY,id_div){
+
+    // Create the chart
+    Highcharts.stockChart(id_div, {
+
+
+        rangeSelector: {
+            selected: 1
+        },
+
+        title: {
+            text: id_div
+        },
+    
+
+        
+
+        series: [{
+            name: id_div,
+            pointStart: Date.now(),
+            pointInterval:1, 
+            
+          data: valoreY,
+            tooltip: {
+                valueDecimals: 2
+            },
+            turboThreshold: 0
         }]
-    },
-   
-});
+       // plotOptions:{ series:{ turboThreshold: 0 } }
+    });
+
 }
-
-
-// const unixTimestamp = 1575909015
-// const milliseconds = 1575909015 * 1000 // 1575909015000
-
-// const dateObject = new Date(milliseconds)
-// const humanDateFormat = dateObject.toLocaleString()
-// console.log(humanDateFormat.slice(11))
-// console.log(dateObject.toLocaleString("en-US", {hour: "numeric"}))
